40dbbe804d6f8c31929ec03e3d6a0f88c30e3699
MelloHyu <adityagupta.iloveneji@gmail.com>
2025-02-20 18:38:01 +0530

    added GPS and some bug fixes

---
+++ Where to Boss/Assets/Scripts/MiniMap.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+
+}
